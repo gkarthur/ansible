7af079fbf139faccf55a35d933dd2d964b55ce7a
gkarthur <gkarthur@gmail.com>
2018-09-26 13:19:56 +0000

    ans update

--- install-ansible-ami.sh
+++ install-ansible-ami.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-sudo pip install ansible
+#sudo pip install ansible
+
+# local config
+sudo mkdir /etc/ansible
+sudo chown ec2-user /etc/ansible
+echo "localhost ansible_connection=local" > /etc/ansible/hosts
